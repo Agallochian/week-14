76431363534b6077bdb34c0f6e5115b2dc56e710
Evan Andreolli <Evan Andreolli>
2016-11-05 23:03:34 -0500

    meh

---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (id INTEGER AUTO_INCREMENT, burger_name VARCHAR(250), devoured BOOLEAN, 'date' TIMESTAMP, PRIMARY KEY (id));
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers_db (burger_name) VALUES ("Cheeseburger");
+INSERT INTO burgers_db (burger_name) VALUES ("Bacon Cheesburger");
+INSERT INTO burgers_db (burger_name) VALUES ("Patty Melt");
